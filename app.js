ee4d53da1eea690132512c408b19c7c8db00fef5
Claude Seguret <claude.seguret@laposte.fr>
2016-04-15 20:59:07 +0200

    remove grunt from production

--- app.js
+++ app.js
@@ -55,5 +55,8 @@ process.chdir(__dirname);
 
 
   // Start server
-  sails.lift(rc('sails'));
+  if (process.env.NODE_ENV === 'production')
+      sails.lift(rc('sails', {hooks:{grunt:false}})
+  else
+      sails.lift(rc('sails'))
 })();
